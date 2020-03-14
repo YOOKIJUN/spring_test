889f3873fd9806cab8f4aff39e892b57daf87a46
yookijun <nice8545@naver.com>
2020-03-14 22:19:58 +0900

    .

--- src/main/java/com/test/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/test/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -29,17 +29,36 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registerationId,
                                      String userNameAttributesName,
                                      Map<String, Object> attributes){
+
+        if("naver".equals(registerationId)){
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributesName, attributes);
     }
 
-    private static OAuthAttributes ofGoogle(String userNameAttributesName,
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+
+        Map<String , Object> response = (Map<String , Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profileImage"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName,
                                             Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
-                .nameAttributeKey(userNameAttributesName)
+                .nameAttributeKey(userNameAttributeName)
                 .build();
     }
 
